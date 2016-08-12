5a343ea4b0a84544dc3e864b400df5fea0b1c19a
Gina Goldberg <gina.goldberg@hotmail.com>
2016-08-13 00:22:37 +0200

    include javascript to animate quiz results

---
+++ javascripts/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  $("#submit").click(function(e) {
+    e.preventDefault();
+
+    var question1 = $("input[name='q1']:checked").val();
+    var question2 = $("input[name='q2']:checked").val();
+    var question3 = $("input[name='q3']:checked").val();
+    var question4 = $("input[name='q4']:checked").val();
+    var question5 = $("input[name='q5']:checked").val();
+
+    if (!(question1) || !(question2) || !(question3) || !(question4) || !(question5))
+      { alert("Je bent nog niet klaar!");}
+
+    else {
+
+      var wrong1 = (question1 != "b");
+      var wrong2 = (question2 != "d");
+      var wrong3 = (question3 != "c");
+      var wrong4 = (question4 != "b");
+      var wrong5 = (question5 != "a");
+      var wrong0 = (!wrong1 && !wrong2 && !wrong3 && !wrong4 && !wrong5);
+
+      var incorrect = [];
+
+      if (wrong1) { incorrect.push("1") };
+      if (wrong2) { incorrect.push("2") };
+      if (wrong3) { incorrect.push("3") };
+      if (wrong4) { incorrect.push("4") };
+      if (wrong5) { incorrect.push("5") };
+      if (wrong0) { incorrect.push("Geen") };
+
+      var resultStr = 'Je hebt de volgende vragen fout: ' + incorrect.join(', ');
+      $("#resultlist").text(resultStr);
+      $("#resultlist").slideToggle("slow");
+
+      if (wrong1) { $("#answer1").slideToggle("slow"); };
+      if (wrong2) { $("#answer2").slideToggle("slow"); };
+      if (wrong3) { $("#answer3").slideToggle("slow"); };
+      if (wrong4) { $("#answer4").slideToggle("slow"); };
+      if (wrong5) { $("#answer5").slideToggle("slow"); };
+      if (wrong0) { $("#answer0").slideToggle("slow"); };
+    }
+  });});
+
+// TODO: slideToggle doesnt work yet... and remove showing answers at all!
+
+// TODO: calculate score and show score
+// TODO: have questions appear after each other?
+// TODO: timer
+// TODO: alert("You have run out of time!")
